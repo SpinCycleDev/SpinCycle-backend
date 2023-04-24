68bb0f0b1d294c2ed8965388d31ab6a51644d027
maxmay94 <maxmay94@gmail.com>
2023-04-24 16:36:23 -0400

    add User table

--- user/models.py
+++ user/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class User(models.Model):
+    USER_CUSTOMER = 'cutomer'
+    USER_SERVICE_PROVIDER = 'service_provider'
+    USER_DRIVER = 'driver'
+    USER_TYPE_CHOICES = (
+        (USER_CUSTOMER, 'Customer'),
+        (USER_SERVICE_PROVIDER, 'Service Provider'),
+        (USER_DRIVER, 'Driver'),
+    )
+
+    user_id = models.CharField(max_length=20, primary_key=True)
+    password = models.CharField(max_length=20)
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    email = models.EmailField()
+    phone_number = models.CharField(max_length=20)
+    user_type = models.CharField(max_length=20, choices=USER_TYPE_CHOICES, default=USER_CUSTOMER)
